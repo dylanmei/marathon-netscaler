df54737f3ae812aa737b499e0e4de86ca1bf5029
Dylan Meissner <dylanmei@gmail.com>
2016-04-27 07:48:05 -0700

    Add/remove netscaler servers

--- netscaler/client.go
+++ netscaler/client.go
@@ -3,8 +3,10 @@ package netscaler
 import (
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	jp "github.com/buger/jsonparser"
 )
@@ -20,6 +22,11 @@ type Client struct {
 	config     *Config
 	httpClient *http.Client
 	version    string
+}
+
+type Server struct {
+	Name string
+	IP   string
 }
 
 func NewClient(config *Config) *Client {
@@ -39,14 +46,9 @@ func (c *Client) Version() (string, error) {
 		return c.version, nil
 	}
 
-	uri := fmt.Sprintf("%v/nitro/v1/config/nsversion", c.config.URL)
-	req, err := http.NewRequest("GET", uri, nil)
+	req, err := c.request("GET", "config/nsversion", nil)
 	if err != nil {
 		return "", err
-	}
-
-	if c.config.HTTPBasicAuthUser != "" {
-		req.SetBasicAuth(c.config.HTTPBasicAuthUser, c.config.HTTPBasicPassword)
 	}
 
 	res, err := c.httpClient.Do(req)
@@ -66,4 +68,101 @@ func (c *Client) Version() (string, error) {
 
 	c.version, err = jp.GetString(body, "nsversion", "version")
 	return c.version, err
+}
+
+func (c *Client) GetServers() ([]Server, error) {
+	servers := []Server{}
+	req, err := c.request("GET", "config/server", nil)
+	if err != nil {
+		return servers, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return servers, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return servers, err
+	}
+
+	if res.StatusCode != 200 {
+		return servers, errors.New(fmt.Sprintf("Unexpected HTTP status: %d", res.StatusCode))
+	}
+
+	err = jp.ArrayEach(body, func(value []byte, dataType jp.ValueType, offset int, err error) {
+		name, err := jp.GetString(value, "name")
+		if err != nil {
+			return
+		}
+		ip, err := jp.GetString(value, "ipaddress")
+		if err != nil {
+			return
+		}
+		servers = append(servers, Server{
+			Name: name,
+			IP:   ip,
+		})
+	}, "server")
+
+	return servers, err
+}
+
+func (c *Client) AddServers(servers ...Server) error {
+	list := []string{}
+	for _, server := range servers {
+		list = append(list,
+			fmt.Sprintf(`{"name": "%s", "ipaddress": "%s"}`, server.Name, server.IP))
+	}
+
+	req, err := c.request("POST", "config/server",
+		strings.NewReader(fmt.Sprintf(`{"server": [%s]}`, strings.Join(list, ","))))
+	req.Header.Set("Content-Type", "application/vnd.com.citrix.netscaler.server_list+json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("Unexpected HTTP status: %d", res.StatusCode))
+	}
+
+	return nil
+}
+
+func (c *Client) RemoveServers(names ...string) error {
+	for _, name := range names {
+		req, err := c.request("DELETE", fmt.Sprintf("config/server/%s", name), nil)
+		if err != nil {
+			return err
+		}
+
+		res, err := c.httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+
+		if res.StatusCode != 200 {
+			return errors.New(fmt.Sprintf("Unexpected HTTP status: %d while deleting %s", res.StatusCode, name))
+		}
+	}
+
+	return nil
+}
+
+func (c *Client) request(method, path string, body io.Reader) (*http.Request, error) {
+	uri := fmt.Sprintf("%v/nitro/v1/%s", c.config.URL, path)
+	req, err := http.NewRequest("GET", uri, body)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.config.HTTPBasicAuthUser != "" {
+		req.SetBasicAuth(c.config.HTTPBasicAuthUser, c.config.HTTPBasicPassword)
+	}
+
+	return req, nil
 }
--- netscaler/client_test.go
+++ netscaler/client_test.go
@@ -2,9 +2,12 @@ package netscaler
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	jp "github.com/buger/jsonparser"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -36,4 +39,104 @@ func Test_netscaler_client_get_version(t *testing.T) {
 
 	require.Nil(err)
 	assert.Equal("foo", version)
+}
+
+func Test_netscaler_find_servers(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/nitro/v1/config/server",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, `{
+				"server": [{
+					"name": "server1",
+					"ipaddress": "1.1.1.1"
+				}, {
+					"name": "server2",
+					"ipaddress": "2.2.2.2"
+				}]
+			}`)
+		})
+
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	config := &Config{
+		URL: server.URL,
+	}
+
+	client := NewClient(config)
+	servers, err := client.GetServers()
+
+	require.Nil(err)
+	require.Equal(2, len(servers))
+
+	assert.Equal("server1", servers[0].Name)
+	assert.Equal("1.1.1.1", servers[0].IP)
+
+	assert.Equal("server2", servers[1].Name)
+	assert.Equal("2.2.2.2", servers[1].IP)
+}
+
+func Test_netscaler_add_servers(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	servers := []string{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/nitro/v1/config/server",
+		func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			body, _ := ioutil.ReadAll(r.Body)
+			jp.ArrayEach(body, func(value []byte, dataType jp.ValueType, offset int, err error) {
+				name, err := jp.GetString(value, "name")
+				if err == nil {
+					servers = append(servers, name)
+				}
+			}, "server")
+		})
+
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	config := &Config{
+		URL: server.URL,
+	}
+	client := NewClient(config)
+	err := client.AddServers(
+		Server{"server1", "1.1.1.1"}, Server{"server2", "2.2.2.2"})
+
+	require.Nil(err)
+	require.Equal(2, len(servers))
+	assert.Equal("server1", servers[0])
+	assert.Equal("server2", servers[1])
+}
+
+func Test_netscaler_remove_servers(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	deleted := []bool{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/nitro/v1/config/server/server1",
+		func(w http.ResponseWriter, r *http.Request) {
+			deleted = append(deleted, true)
+		})
+	mux.HandleFunc("/nitro/v1/config/server/server2",
+		func(w http.ResponseWriter, r *http.Request) {
+			deleted = append(deleted, true)
+		})
+
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	config := &Config{
+		URL: server.URL,
+	}
+	client := NewClient(config)
+	err := client.RemoveServers("server1", "server2")
+
+	require.Nil(err)
+	assert.Equal(2, len(deleted))
 }
